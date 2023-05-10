04a9cd70c26cc1d52fdca479b675c9c79c0e86d1
emi-rai <emilyarai@gmail.com>
2023-05-10 19:36:11 -0400

    initial commit

---
+++ src/components/Projects.js
@@ -0,0 +1,47 @@
+import { CodeIcon } from "@heroicons/react/solid";
+import React from "react";
+import { projects } from "../data";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-10">
+          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+            Apps I've Built
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            I love the creative process of designing and building applications, both individually or as a part of a team.
+          </p>
+        </div>
+        <div className="h-full w-2/3 m-auto">
+          {projects.map((project) => (
+            <a
+              href={project.link}
+              key={project.image}
+              target='_blank'
+              className="sm:w-1/2 no-wrap w-40 p-4">
+              <div className="flex relative mb-8">
+                <img
+                  alt="gallery"
+                  className="absolute inset-0 w-full h-full object-center rounded-md"
+                  src={project.image}
+                />
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                    {project.title}
+                  </h1>
+                  <p className="leading-relaxed">{project.description}</p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,71 @@
+import shapeshifters from './assets/ShapeShifters.gif'
+import hikeNE from './assets/HikeNE.gif'
+import hike67 from './assets/hike-the-67.gif'
+import aeauto from './assets/ae-auto.gif'
+import diana from './assets/diana_linkedin.jpeg'
+import alix from './assets/alix_linkedin.jpeg'
+
+export const projects = [
+  {
+    title: "ShapeShifters",
+    subtitle: "FastAPI, Redux, Tailwind",
+    description:
+      "Meet new people with common interests and get active with ShapeShifters ",
+    image: shapeshifters,
+    link: "https://gitlab.com/Alix-de-Pannemaecker/AE-Autos",
+  },
+  {
+    title: "Hike the 67",
+     subtitle: "Django, HTML, CSS",
+    description:
+      "Keep a blog style log of hikes while tracking progress completing the 67 New England 4000+ footers ",
+    image: hike67,
+    link: "https://gitlab.com/emi.rai/hike-the-67",
+  },
+  {
+    title: "AE Auto",
+    subtitle: "Django, React",
+    description:
+      "Web application for automobile sales, rental, and service business",
+    image: aeauto,
+    link: "https://gitlab.com/Alix-de-Pannemaecker/AE-Autos",
+  },
+  {
+    title: "HikeNE",
+    subtitle: "Flask, Jinja, Bootstrap",
+    description:
+      "Journal all your hikes and track your progress with completing hikes. ",
+    image: hikeNE,
+    link: "https://www.youtube.com/watch?v=x7B65BOGUzE",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "She was an outstanding team player who effectively communicated with me as we worked on a project, ensuring we were always on the same page. Emily's ability to identify the root cause of problems and come up with innovative solutions to resolve them was truly inspiring. Her debugging skills were exceptional, and she always made me feel comfortable asking questions or seeking help whenever I needed it.",
+    image: diana,
+    name: "Diana Angarita",
+    company: "Hack Reactor",
+    link: "https://www.linkedin.com/in/diana-angaritah/"
+  },
+  {
+    quote:
+      "I had the pleasure to work with Emily on a full-stack application development during module 2 of the Hack Reactor intensive coding bootcamp. Always keeping a positive spirit, Emily is a hard-working engineer, and I was impressed by her capacity to learn and not being stuck by any issue we encountered during development. It is also worth mentioning that Emily has an artistic touch and is highly skilled as a front-end developer.",
+    image: alix,
+    name: "Alix de Pannemaecker",
+    company: "Hack Reactor",
+    link: "https://www.linkedin.com/in/alix-de-pannemaecker/",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Python",
+  "Django",
+  "FastAPI",
+  "CSS",
+  "PostgreSQL",
+  "Redux"
+];
