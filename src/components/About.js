ca5a051630eb00fbf1e96c43914a8a3d441abd86
emi-rai <emilyarai@gmail.com>
2023-06-06 12:06:34 -0400

    working on form, installed emailjs/browser

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import profile from "../assets/profile.jpeg"
 
 export default function About() {
     return (
--- src/data.js
+++ src/data.js
@@ -1,9 +1,10 @@
-import shapeshifters from './assets/ShapeShifters.gif'
-import hikeNE from './assets/HikeNE.gif'
-import hike67 from './assets/hike-the-67.gif'
-import aeauto from './assets/ae-auto.gif'
-import diana from './assets/diana_linkedin.jpeg'
-import alix from './assets/alix_linkedin.jpeg'
+// import shapeshifters from './assets/ShapeShifters.gif'
+// import hikeNE from './assets/HikeNE.gif'
+// import hike67 from './assets/hike-the-67.gif'
+// import aeauto from './assets/ae-auto.gif'
+// import diana from './assets/diana_linkedin.jpeg'
+// import alix from './assets/alix_linkedin.jpeg'
+
 
 export const projects = [
   {
@@ -11,7 +12,7 @@ export const projects = [
     subtitle: "FastAPI, Redux, Tailwind",
     description:
       "Meet new people with common interests and get active with ShapeShifters ",
-    image: shapeshifters,
+    image: "https://i.imgur.com/ji1dHy6.gif",
     link: "https://gitlab.com/Alix-de-Pannemaecker/AE-Autos",
   },
   {
@@ -19,7 +20,7 @@ export const projects = [
     subtitle: "Flask, Jinja, Bootstrap",
     description:
       "Journal all your hikes and track your progress with completing hikes. ",
-    image: hikeNE,
+    image: "https://i.imgur.com/TSgJ91X.gif",
     link: "https://www.youtube.com/watch?v=x7B65BOGUzE",
   },
   {
@@ -27,7 +28,7 @@ export const projects = [
     subtitle: "Django, HTML, CSS",
     description:
      "Keep a blog style log of hikes while tracking progress completing the 67 New England 4000+ footers ",
-    image: hike67,
+    image: "https://i.imgur.com/cYqK97r.gif",
     link: "https://gitlab.com/emi.rai/hike-the-67",
   },
   {
@@ -35,7 +36,7 @@ export const projects = [
     subtitle: "Django, React",
     description:
       "Web application for automobile sales, rental, and service business",
-    image: aeauto,
+    image: "https://i.imgur.com/iTBoGJM.gif",
     link: "https://gitlab.com/Alix-de-Pannemaecker/AE-Autos",
   },
 ];
@@ -44,7 +45,7 @@ export const testimonials = [
   {
     quote:
       "She was an outstanding team player who effectively communicated with me as we worked on a project, ensuring we were always on the same page. Emily's ability to identify the root cause of problems and come up with innovative solutions to resolve them was truly inspiring. Her debugging skills were exceptional, and she always made me feel comfortable asking questions or seeking help whenever I needed it.",
-    image: diana,
+    image: "https://i.imgur.com/aZXFtUr.jpg",
     name: "Diana Angarita",
     company: "Hack Reactor",
     link: "https://www.linkedin.com/in/diana-angaritah/"
@@ -52,7 +53,7 @@ export const testimonials = [
   {
     quote:
       "I had the pleasure to work with Emily on a full-stack application development during module 2 of the Hack Reactor intensive coding bootcamp. Always keeping a positive spirit, Emily is a hard-working engineer, and I was impressed by her capacity to learn and not being stuck by any issue we encountered during development. It is also worth mentioning that Emily has an artistic touch and is highly skilled as a front-end developer.",
-    image: alix,
+    image: "https://i.imgur.com/in4V2bO.jpg",
     name: "Alix de Pannemaecker",
     company: "Hack Reactor",
     link: "https://www.linkedin.com/in/alix-de-pannemaecker/",
@@ -67,5 +68,7 @@ export const skills = [
   "FastAPI",
   "CSS",
   "PostgreSQL",
-  "Redux"
+  "Redux",
+  "Docker",
+  "CI/CD"
 ];
